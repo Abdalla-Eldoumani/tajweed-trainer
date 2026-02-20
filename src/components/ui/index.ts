b00a2158cdedc61e11ff49109a4000cab24cc475
Abdalla Eldoumani <aamsdoumani@gmail.com>
2026-02-20 04:21:13 -0700

    Add UI components barrel export

---
+++ src/components/ui/index.ts
@@ -0,0 +1,7 @@
+export { Button } from "./Button";
+export { Card } from "./Card";
+export { Badge } from "./Badge";
+export { ProgressBar } from "./ProgressBar";
+export { ArabicText } from "./ArabicText";
+export { TajweedText } from "./TajweedText";
+export { AudioPlayer } from "./AudioPlayer";
