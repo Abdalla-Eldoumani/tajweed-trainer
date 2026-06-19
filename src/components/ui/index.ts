d1f7f927024a8f31cebc7930240e20a53cc38e22
Abdalla Eldoumani <aamsdoumani@gmail.com>
2026-06-19 21:10:16 +0300

    delete orphaned components and ui barrel

--- src/components/ui/index.ts
+++
@@ -1,8 +0,0 @@
-export { Button } from "./Button";
-export { Card } from "./Card";
-export { Badge } from "./Badge";
-export { SectionHeading } from "./SectionHeading";
-export { ProgressBar } from "./ProgressBar";
-export { ArabicText } from "./ArabicText";
-export { TajweedText } from "./TajweedText";
-export { AudioPlayer } from "./AudioPlayer";
